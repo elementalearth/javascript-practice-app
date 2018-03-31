686435d5f184bd7439d0290bc4cedef76aa07a41
Jimmy Cleveland <jimmydalecleveland@gmail.com>
2018-03-31 17:10:01 -0600

    Move app.js logic to view object

--- src/todolist.js
+++ src/todolist.js
@@ -1,6 +1,16 @@
 // Creates the Todo list and sets completed to false for each todo in the array
+// (function() {
+
 var todoList = {
   todos: [],
+  init: function() {
+    todoList.add('first');
+    todoList.add('second');
+    todoList.add('third');
+    todoList.update(0, 'not first anymore');
+    todoList.toggle(0);
+  },
+
   print: function(caller) {
     if (this.todos.length === 0) {
       console.log('You have no todos');
@@ -15,6 +25,7 @@ var todoList = {
       }
     }
   },
+
   // Adds a todo at the end of the array and sets completed to false
   add: function(todoText) {
     this.todos.push({
@@ -22,22 +33,28 @@ var todoList = {
       completed: false,
     });
   },
+
   //Removes a todo when the 'delete' button is pressed
   remove: function(pos) {
     //position of the parentNode which comes from the delete-button click eventListener in script.js
     this.todos.splice(pos, 1); //cuts out the <li> at only (pos, 1) the position clicked in the array
   },
+
   // Updates a todo
   update: function(pos, newText) {
     //all new inputs are stored in newText
     this.todos[pos].todoText = newText; //sets todoText as equal to newText
     this.print('Updated a todo');
   },
+
   // Toggles the 'completed' element when the li is clicked
   toggle: function(pos) {
     this.todos[pos].completed = !this.todos[pos].completed; //flips completed class when function is called
     this.print('Toggled a todo');
   },
 };
+
+todoList.init();
+// })()
 
 export default todoList;
--- src/view.js
+++ src/view.js
@@ -3,7 +3,10 @@ import todoList from './todolist';
 //Object that writes the todo list data to the DOM
 var view = {
   render: function() {
-    todoContainer.innerHTML = ''; //sets DOM to blank
+    this.cacheDOM();
+    this.bindClick();
+
+    this.todoContainer.innerHTML = ''; //sets DOM to blank
     var todos = todoList.todos; //sets todos equal to todos array in todolist.js
 
     todos.forEach(function(todo, pos) {
@@ -43,16 +46,41 @@ var view = {
       todoContainer.appendChild(li); //appends button to each li created in todoContainer
     });
   },
+
+  cacheDOM: function() {
+    this.todoContainer = document.querySelector('#todoContainer');
+  },
+
+  bindClick: function() {
+    todoContainer.addEventListener('click', function(e) {
+      //adds eventlistener to DOM and looks for a click event. passes click event into function as 'e'
+      var targetClasses = e.target.classList; //var that simplifies target id and reduces typing out extra lines for event if statements
+
+      if (targetClasses.contains('delete-button')) {
+        //if event target classlist contains the 'delete-button' class view.js deleteTodo function is called
+        view.deleteTodo(e.target.parentNode.id); //passes location if 'delete-button' id to delete function in view.js which deletes the todo located at the event target by removing the parentnode (li) by id from the array
+      } else if (targetClasses.contains('toggle-button')) {
+        //if event target classlist contains the 'toggle-button' class then view.js toggleTodo function is called
+        view.toggleTodo(e.target.parentNode.id); //passes location to toggle function in view.js which is the event target id (toggle-todo) parentNode which is the <li>
+      } else if (targetClasses.contains('todo-text')) {
+        //if event target class contains the todo-text class then the editTodo function in view.js is called
+        view.editTodo(e.target.parentNode); //passes the event target parentNode <li> to view.editTodo
+      }
+    });
+  },
+
   deleteTodo: function(pos) {
     //removes a todo based off click position listed in event listener in script.js
     todoList.remove(pos); //calls function in todolist.js
     this.render(); //calls render function and writes data to DOM
   },
+
   toggleTodo: function(pos) {
     //adds item-completed class a todo based off click position listed in event listener in script.js
     todoList.toggle(pos); //calls function in todolist.js
     this.render(); //calls render function and writes data to DOM
   },
+
   editTodo: function(targetParent) {
     //edits the text in a todo
     targetParent.classList.add('edit'); //adds the 'edit' class for use in .css which then hides the span and replaces it with the input class
@@ -60,10 +88,12 @@ var view = {
     input.focus(); //focuses the target input for use with the updateTodo function in view.js when key 13 (Enter) is pressed
     input.setSelectionRange(0, input.value.length); //automatically highlights the text in todo
   },
+
   deselectEdit: function(e) {
     //deselects a todo and reverts the .css
     e.target.parentNode.classList.remove('edit'); //removes the 'edit' class from the parentNode <li> which reverts the .css for the 'input' and 'span' classes to their normal state ***This will not cause the text in a todo to update! Key 13 (Enter) must be pressed for that to happen***
   },
+
   updateTodo: function(e) {
     //updates the text in a todo item
     if (e.keyCode === 13) {
