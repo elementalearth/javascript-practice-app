6aae850652316ece5cb83a885d101f0aaa458dd7
Jimmy Cleveland <jimmydalecleveland@gmail.com>
2018-04-08 11:17:06 -0600

    Add swapi widget

--- src/app.js
+++ src/app.js
@@ -1,2 +1,13 @@
 import view from './view';
 view.render();
+
+const shipTag = document.querySelector('#shipTag');
+
+fetch('https://swapi.co/api/starships/10/')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    shipTag.innerHTML = myJson.name;
+    console.log(myJson);
+  });
