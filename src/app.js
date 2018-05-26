0b048e2ea09a095e365321b9cff15fdaa0a33761
Jimmy Cleveland <jimmydalecleveland@gmail.com>
2018-05-25 23:06:02 -0600

    Add ships pilots (currently logging)

--- src/app.js
+++ src/app.js
@@ -1,13 +1,23 @@
-import view from './view';
+import view from "./view";
 view.render();
 
-const shipTag = document.querySelector('#shipTag');
+const shipTag = document.querySelector("#shipTag");
+const pilots = {};
 
-fetch('https://swapi.co/api/starships/10/')
+fetch("https://swapi.co/api/starships/10/")
   .then(function(response) {
     return response.json();
   })
   .then(function(myJson) {
+    myJson.pilots.forEach(function(pilot) {
+      fetch(pilot)
+        .then(function(pilotRes) {
+          return pilotRes.json();
+        })
+        .then(function(pilotJson) {
+          console.log(pilotJson);
+        });
+    });
+
     shipTag.innerHTML = myJson.name;
-    console.log(myJson);
   });
---
+++ src/logger.js
@@ -0,0 +1,5 @@
+function logger() {
+  console.log("I am the logger!");
+}
+
+export default logger;
