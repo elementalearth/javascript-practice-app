871acd5f8f7b92f37ee00891e1e083419f2d3937
Jacob <vernonjacoba@yahoo.com>
2018-06-01 20:52:20 -0600

    ITS WORKING

--- src/app.js
+++ src/app.js
@@ -2,6 +2,12 @@ import view from "./view";
 view.render();
 
 const shipTag = document.querySelector("#shipTag");
+const shipModel = document.querySelector("#shipModel");
+const shipSpeed = document.querySelector("#shipSpeed");
+const shipCrew = document.querySelector("#shipCrew");
+const shipPassengers = document.querySelector("#shipPassengers");
+const shipPilots = document.querySelector("#shipPilots");
+const knownPilots = document.querySelector("#knownPilots");
 const pilots = {};
 
 fetch("https://swapi.co/api/starships/10/")
@@ -16,8 +22,19 @@ fetch("https://swapi.co/api/starships/10/")
         })
         .then(function(pilotJson) {
           console.log(pilotJson);
+          console.log(pilotJson.name);
+          //pilotJson.name.forEach(function(){
+            var li = document.createElement('li');
+            li.innerHTML = pilotJson.name;
+            knownPilots.appendChild(li);
+        //  });
         });
     });
-
     shipTag.innerHTML = myJson.name;
+    shipModel.innerHTML = myJson.model;
+    shipSpeed.innerHTML = myJson.max_atmosphering_speed;
+    shipCrew.innerHTML = myJson.crew;
+    shipPassengers.innerHTML = myJson.passengers;
+  //shipPilots.innerHTML = myJson.pilots;
+  //pilots.innerHTML = myJson.name;
   });
